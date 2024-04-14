5102da62cd51dd41aac038aebb058542a540bf7a
Shashank <97978650+shash0708@users.noreply.github.com>
2024-04-14 12:19:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const ResumeRoutes=require('./routes/index');
 const app = express();
 const uri="mongodb+srv://Rohi:EgWbIbD27nNOQipJ@cluster0.eqsiiyv.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(uri);
-// mongodb+srv://shashankpeddinti07:rCY9I6Bzqb7gNWA9@cluster0.urkxeqa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 const con=mongoose.connection;
 con.once("open", () => {
   console.log("Mongo DB connected");
@@ -18,8 +17,23 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/create-pdf", (req, res) => {
-  pdf.create(pdfSample(req.body), {}).toFile("Resume.pdf", (err) => {
+const { template1, template2 } = require('./pdf-sample/index');
+
+app.post("/create-pdf/:id", (req, res) => {
+  const {  data } = req.body;
+  const { id } = req.params; // Extract id from route parameters
+
+  // Select template based on id
+  let selectedTemplate;
+  if (id === 'template1') {
+    selectedTemplate = template1;
+  } else if (id === 'template2') {
+    selectedTemplate = template2;
+  } else {
+    return res.status(400).send("Invalid template id");
+  }
+
+  pdf.create(selectedTemplate(data), {}).toFile("Resume.pdf", (err) => {
     if (err) {
       res.send(Promise.reject());
       console.log(err);
@@ -42,4 +56,3 @@ app.use(express.static("../client/build"));
   app.listen(port, () => {
     console.log(`server is running on port:${port}`);
   });
-
